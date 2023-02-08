f1dcff11af6a053e43c235491eb02dda31803a4c
Trần Quốc Nghĩa 21011618 <garenpc2003@gmail.com>
2023-02-08 21:59:05 +0700

    Create player movement with the legacy input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private void Update()
+    {
+        var inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+        inputVector = inputVector.normalized;
+        var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDirection * _movementSpeed * Time.deltaTime;
+    }
+}
